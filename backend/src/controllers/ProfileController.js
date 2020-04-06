55b46ca6e67850ee569fb98bac9cb96a76e46c68
Matheus Eduardo Barboza Santos <38351837+matheuseduardo09@users.noreply.github.com>
2020-04-05 21:42:51 -0300

    Initial commit

---
+++ backend/src/controllers/ProfileController.js
@@ -0,0 +1,19 @@
+const connection = require("../database/connection");
+
+module.exports = {
+  async index(request, response) {
+    const ong_id = request.headers.authorization;
+
+    if (!ong_id) {
+      return response.status(401).json({
+        error: "Authorization not found"
+      });
+    }
+
+    const incidents = await connection("incidents")
+      .where("ong_id", ong_id)
+      .select("*");
+
+    return response.json(incidents);
+  }
+};
